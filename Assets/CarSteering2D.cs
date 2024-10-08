abf98ae78eb5d835499f0071c0ea44515c52ab2a
leo <leoyng95@gmail.com>
2024-10-08 19:59:57 +0200

    carSteering_roule_touche

---
+++ Assets/CarSteering2D.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSteering2D : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float accelerationPower = 10f;
+    [SerializeField]
+    float steeringPower = -10f;
+    float steeringAmount, speed, direction;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        steeringAmount = -Input.GetAxis("Horizontal");
+        speed = Input.GetAxis("Vertical") * accelerationPower;
+        direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
+        rb.rotation += steeringAmount * steeringPower * rb.velocity.magnitude * direction;
+
+        rb.AddRelativeForce(Vector2.up * speed);
+
+        rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+    }
+}
