22fa37135417a6770ad18edf5db3f4240ce4c651
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2024-10-09 13:43:05 +0200

    fix cs

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -14,7 +14,7 @@ public class Options : MonoBehaviour
     public Slider slider;
     public Text TxtVolume;
 
-    private void Start()=> SliderChange();
+    private void Start() => SliderChange();
     
     void Update()
     {
@@ -26,39 +26,38 @@ public class Options : MonoBehaviour
     }
 
     public void SetResolution()
-{
-    switch (DResolution.value)
     {
-        case 0:
-            Screen.SetResolution(640, 360, true);
-            break;
-        case 1:
-            Screen.SetResolution(1024, 768, true);
-            break;
-        case 2:
-            Screen.SetResolution(1280, 800, true);
-            break;
-        case 3:
-            Screen.SetResolution(1280, 1024, true);
-            break;
-        case 4:
-            Screen.SetResolution(1600, 1200, true);
-            break;
-        case 5:
-            Screen.SetResolution(1680, 1050, true);
-            break;
-        case 6:
-            Screen.SetResolution(1920, 1080, true);
-            break;
-        case 7:
-            Screen.SetResolution(1920, 1200, true);
-            break;
-        case 8:
-            Screen.SetResolution(2560, 1600, true);
-            break;
+        switch (DResolution.value)
+        {
+            case 0:
+                Screen.SetResolution(640, 360, true);
+                break;
+            case 1:
+                Screen.SetResolution(1024, 768, true);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 800, true);
+                break;
+            case 3:
+                Screen.SetResolution(1280, 1024, true);
+                break;
+            case 4:
+                Screen.SetResolution(1600, 1200, true);
+                break;
+            case 5:
+                Screen.SetResolution(1680, 1050, true);
+                break;
+            case 6:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+            case 7:
+                Screen.SetResolution(1920, 1200, true);
+                break;
+            case 8:
+                Screen.SetResolution(2560, 1600, true);
+                break;
+        }
     }
-}
-
 
     public void SliderChange()
     {
