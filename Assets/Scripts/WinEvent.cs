b46614d8fdb1bc9dc5f20152f121f89e02e198a7
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2024-10-10 21:41:05 +0200

    add win player

---
+++ Assets/Scripts/WinEvent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinEvent : MonoBehaviour
+{
+    public int lapsCompleted = 0;
+    public int lapsToWin = 3;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            lapsCompleted++;
+            Debug.Log("Passage sur la ligne de victoire : " + lapsCompleted + "/" + lapsToWin);
+
+            if (lapsCompleted >= lapsToWin)
+            {
+                Debug.Log("Victoire ! Le joueur a gagné la course.");
+                WinRace();
+            }
+        }
+    }
+
+    private void WinRace()
+    {
+        Debug.Log("Le joueur a remporté la course !");
+    }
+}
